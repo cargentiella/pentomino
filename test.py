825b3b74b1c169b18c35103de69f8cc67f146bae
cargentiella <vagrant@spica.sphere.local>
2019-01-05 07:41:08 +0000

    20190105 1641

--- test.py
+++ test.py
@@ -3,42 +3,104 @@ import sys
 
 # -------- init --------
 
-class cell:
-	EMPTY = '\033[30m'
-	FRAME = '\033[37m'
-	END = '\033[0m'
-
-
 case_with = 6
 case_height = 10
+
+blocks = ['i', 'l', 'n', 'y', 'f', 'p', 't', 'u', 'v', 'w', 'x', 'z']
+	
+block = {
+	'i' : [	[1, 1, 1, 1, 1], 
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0]	],
+	'l' : [	[1, 1, 1, 1],
+		[1, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]	],
+	'n' : [	[0, 1, 1, 1],
+		[1, 1, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]	],
+	'y' : [	[1, 1, 1, 1],
+		[0, 1, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]	],
+	'f' : [	[0, 1, 1],
+		[1, 1, 0],
+		[0, 1, 0]	],
+	'p' : [	[1, 1, 1],
+		[1, 1, 0],
+		[0, 0, 0]	],
+	't' : [	[1, 1, 1],
+		[0, 1, 0],
+		[0, 1, 0]	],
+	'u' : [	[1, 1, 1],
+		[1, 0, 1],
+		[0, 0, 0]	],
+	'v' : [	[1, 1, 1],
+		[1, 0, 0],
+		[1, 0, 0]	],
+	'w' : [	[1, 1, 0],
+		[0, 1, 1],
+		[0, 0, 1]	],
+	'x' : [	[0, 1, 0],
+		[1, 1, 1],
+		[0, 1, 0]	],
+	'z' : [	[1, 1, 0],
+		[0, 1, 0],
+		[0, 1, 1]	]
+}
+
+pullet = {
+	'EMPTY' : '\033[30m',
+	'i' : '\033[31m',
+	'l' : '\033[35m',
+	'n' : '\033[32m',
+	'y' : '\033[37m',
+	'f' : '\033[37m',
+	'p' : '\033[36m',
+	't' : '\033[35m',
+	'u' : '\033[36m',
+	'v' : '\033[31m',
+	'w' : '\033[33m',
+	'x' : '\033[33m',
+	'z' : '\033[32m',
+	'END' : '\033[0m'
+}
+
 
 
 
 # -------- func --------
 
 def display_case(case):
-        for j in range(case_height + 2):
-                for i in range(case_with + 2):
-                        sys.stdout.write(case[j][i] + "■" + cell.END)
+        for j in range(case_height):
+                for i in range(case_with):
+                        sys.stdout.write(case[j][i] + "■" + pullet['END'])
 		sys.stdout.write("\n")
+
+def put_block(case, locate, block, color):
+	for j in range(len(block)):
+		for i in range(len(block[j])):
+			if block[j][i]:
+				case[locate['y'] + j][locate['x'] + i] = color
+	return case
+
 
 # -------- main --------
 
+locate = {
+	'x' : 0,
+	'y' : 0
+}
 
-case = [[cell.EMPTY for i in range(case_with + 2)] for j in range(case_height + 2)]
+case = [[pullet['EMPTY'] for i in range(case_with)] for j in range(case_height)]
 
-for i in range(case_with + 2):
-	case[0][i] = cell.FRAME
-	case[case_height + 1][i] = cell.FRAME
-
-for j in range(case_height):
-	case[j + 1][0] = cell.FRAME
-	case[j + 1][case_with + 1] = cell.FRAME
+caset = put_block(case, locate, block['v'], pullet['v'])
 
 
 
-
-#print(case)
 display_case(case)
 
 
